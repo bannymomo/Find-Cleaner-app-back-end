9b62423005c67d8ac0a782acf23d8d02372e2a21
bannymomo <bannymomo33@gmail.com>
2020-02-03 16:41:01 +1000

    v3.1---create-basic-business-model

---
+++ src/models/business.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const schema = new mongoose.Schema({
+  businessName: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  postcode: {
+    type: Number,
+    required: true
+  },
+  telephoneNumber: {
+    type: Number,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  memberSince: {
+    type: Date,
+    default: Date.now
+  },
+  lastOnline: {
+    type: Date,
+    default: Date.now
+  },
+  photo: {
+    type: String, //url
+    default:
+      "https://library.kissclipart.com/20180929/vpq/kissclipart-icon-clipart-computer-icons-business-company-6438dbeb59c64e42.jpg"
+  },
+  description: {
+    type: String,
+    default: "Find Cleaner app business"
+  }
+});
+
+const model = mongoose.model("Business", schema);
+module.exports = model;
