060d764c25e0c020f673624af7f5d68cab4ee236
bannymomo <bannymomo33@gmail.com>
2020-02-03 21:23:59 +1000

    v3.3---update-client-controller

--- src/controllers/client.js
+++ src/controllers/client.js
@@ -1,8 +1,19 @@
-function addClient(req, res) {}
-function getClient(req, res) {}
-function getAllClients(req, res) {
-  res.json("All Clients~~~~~");
+const Client = require("../models/client");
+async function addClient(req, res) {
+  const client = new Client({
+    firstName: "banny",
+    lastName: "momo",
+    gender: "female",
+    age: 30,
+    email: "aqua@126.com",
+    postcode: 4000
+  });
+  await client.save();
+  return res.json(client);
 }
+
+function getClient(req, res) {}
+function getAllClients(req, res) {}
 function updateClient(req, res) {}
 function deleteClient(req, res) {}
 
