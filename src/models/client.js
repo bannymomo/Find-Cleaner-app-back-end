62d18b1b4e1c110bf76016e5a9091fb6bd18b6bc
bannymomo <bannymomo33@gmail.com>
2020-02-03 22:11:49 +1000

    v5.0---virtuals

--- src/models/client.js
+++ src/models/client.js
@@ -1,47 +1,54 @@
 const mongoose = require("mongoose");
-const schema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true
+const schema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    gender: {
+      type: String,
+      required: true
+    },
+    age: {
+      type: Number,
+      min: 10,
+      max: 95,
+      default: 20
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    postcode: {
+      type: Number,
+      required: true
+    },
+    memberSince: {
+      type: Date,
+      default: Date.now
+    },
+    lastOnline: {
+      type: Date,
+      default: Date.now
+    },
+    photo: {
+      type: String, //url
+      default: "http://www.pngmart.com/files/10/User-Account-PNG-Clipart.png"
+    },
+    description: {
+      type: String,
+      default: "Find Cleaner app client"
+    }
   },
-  lastName: {
-    type: String,
-    required: true
-  },
-  gender: {
-    type: String,
-    required: true
-  },
-  age: {
-    type: Number,
-    min: 10,
-    max: 95,
-    default: 20
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  postcode: {
-    type: Number,
-    required: true
-  },
-  memberSince: {
-    type: Date,
-    default: Date.now
-  },
-  lastOnline: {
-    type: Date,
-    default: Date.now
-  },
-  photo: {
-    type: String, //url
-    default: "http://www.pngmart.com/files/10/User-Account-PNG-Clipart.png"
-  },
-  description: {
-    type: String,
-    default: "Find Cleaner app client"
-  }
+  { toJSON: { virtuals: true }, id: false }
+);
+
+schema.virtual("fullname").get(function() {
+  return `${this.firstName} ${this.lastName}`;
 });
 
 const model = mongoose.model("Client", schema);
