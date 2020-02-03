a82f748eb801b4aa9db6c9a42988f19a9ab9844d
bannymomo <bannymomo33@gmail.com>
2020-02-03 15:21:48 +1000

    v1.1---add-client-route

---
+++ src/controllers/client.js
@@ -0,0 +1,13 @@
+function addClient(req, res) {}
+function getClient(req, res) {}
+function getAllClients(req, res) {}
+function updateClient(req, res) {}
+function deleteClient(req, res) {}
+
+module.exports = {
+  addClient,
+  getClient,
+  getAllClients,
+  updateClient,
+  deleteClient
+};
