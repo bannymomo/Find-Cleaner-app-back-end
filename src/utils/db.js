97152d92880517ff587279def83944b5fc69abc1
bannymomo <bannymomo33@gmail.com>
2020-02-22 14:31:39 +1000

    add .npmrc and connect to  mongoDB atlas database

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
 function connectToDB() {
-  const { DB_HOST, DB_PORT, DB_DATABASE } = process.env;
-  const connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+  const { DB_HOST, DB_PORT, DB_DATABASE, DB_USER, DB_PASSWORD } = process.env;
+  let connectionString;
+  if (DB_USER && DB_PASSWORD) {
+    connectionString = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DATABASE}`;
+  } else {
+    connectionString = `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
+  }
   return mongoose.connect(connectionString, {
     useUnifiedTopology: true,
     useNewUrlParser: true
